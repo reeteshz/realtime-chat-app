5bf05640df36e5f05cc1003eb7090a9e015c6554
reetesh13 <reeteshzope@gmail.com>
2022-05-13 13:39:16 -0400

    Message model created and message socket connection established

--- server/app.js
+++ server/app.js
@@ -1,8 +1,43 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const path = require('path');
+const io = require('socket.io')(http);
+
+const uri = process.env.MONGODB_URI;
+const port = process.env.PORT || 3000;
+
+const Message = require('./Message');
+const mongoose = require('mongoose');
+
+mongoose.connect(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
-app.listen(3000, function () {
-  console.log('Realtime chat is listening on port 3000!');
+
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+io.on('connection', (socket) => {
+
+  Message.find().sort({createdAt: -1}).limit(10).exec((err, messages) => {
+    if (err) return console.error(err);
+    socket.emit('init', messages);
+  });
+
+  socket.on('message', (msg) => {
+    const message = new Message({
+      content: msg.content,
+      name: msg.name,
+    });
+
+    message.save((err) => {
+      if (err) return console.error(err);
+    });
+
+    socket.broadcast.emit('push', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log('Realtime app is listening on port:' + port);
 });
